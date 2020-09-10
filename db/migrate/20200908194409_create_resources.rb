2cdd60d9619de28852d2b2f40fc0044a12d344cb
breewarren <ambreea.warren@gmail.com>
2020-09-09 21:24:12 -0500

    migration and seed file

--- db/migrate/20200908194409_create_resources.rb
+++ db/migrate/20200908194409_create_resources.rb
@@ -1,7 +1,7 @@
 class CreateResources < ActiveRecord::Migration[6.0]
   def change
     create_table :resources do |t|
-      t.string :type
+      t.string :resource_type
       t.string :details
     end
   end
